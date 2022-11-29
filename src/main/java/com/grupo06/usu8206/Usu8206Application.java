5aa59133d92adb704095b59507392e9010a6dae9
NoeliaxRodriguez <100428957@alumnos.uc3m.es>
2022-11-29 19:56:38 +0100

    Initial commit

---
+++ src/main/java/com/grupo06/usu8206/Usu8206Application.java
@@ -0,0 +1,13 @@
+package com.grupo06.usu8206;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Usu8206Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Usu8206Application.class, args);
+    }
+
+}
